10739530334434a083b2ad5a9f94ebda7b643316
paul <paul.guirbal@outlook.fr>
2017-12-14 17:33:38 +0100

    [recognition] début de fonction pour traçage du contour

--- software/recognize.py
+++ software/recognize.py
@@ -2,3 +2,12 @@ import numpy as np
 import cv2
 import glob, os, sys, time, datetime
 
+
+
+# Function drawing the contour of the animal with the current image
+def drawContour(image):
+    # We retrieve the heigth and the width of the image
+    height = image.getHeight()
+    width = image.getWidth()
+
+    
