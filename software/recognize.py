8522e146a40b1d9aad556b4f0abe5e69c372e9f1
paul <paul.guirbal@outlook.fr>
2018-01-08 15:30:36 +0100

    [recognition] Ajout de background substractor + blob finding

--- software/recognize.py
+++ software/recognize.py
@@ -5,23 +5,41 @@ import cv2
 import glob, os, sys, time, datetime
 
 
+cap = cv2.VideoCapture('Test_lepton.mp4')
 
-# Function drawing the contour of the animal with the current image
-def drawContour(image):
-    # We retrieve the heigth and the width of the image
-    height = image.getHeight()
-    width = image.getWidth()
+#création du masque pour enlever le background
+fgbg = cv2.createBackgroundSubtractorMOG2()
 
+# Set up the detector with default parameters.
+detector = cv2.SimpleBlobDetector_create()
 
-cap = cv2.VideoCapture('Test_lepton.avi')
-
-fgbg = cv2.createBackgroundSubtractorMOG()
 while(1):
+    #capture de l'image de la video
     ret, frame = cap.read()
+
+    #soustraction du background et obtention d'un nouvelle image filtrée
     gmask = fgbg.apply(frame)
-    cv2.imshow('frame',fgmask)
+
+    # Detecte les blobs
+    keypoints = detector.detect(gmask)
+    # Draw detected blobs as red circles.
+    # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
+    im_with_keypoints = cv2.drawKeypoints(gmask, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+    contours,hierarchy = cv2.findContours(gmask, 1, 2)
+    cnt = contours[0]
+    epsilon = 0.1*cv2.arcLength(cnt,True)
+    approx = cv2.approxPolyDP(cnt,epsilon,True)
+
+    #rect = cv2.minAreaRect(cnt)
+    box = cv2.boxPoints(approx)
+    box = np.int0(box)
+    cv2.drawContours(gmask,[box],0,(0,0,255),2)
+
+    #Affichage de l'image
+    cv2.imshow('frame',gmask)
     k = cv2.waitKey(30) & 0xff
-    if k == 27:
+    if k == 's':
         break
 
 cap.release()
