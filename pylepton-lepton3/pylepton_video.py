0a64ae9256a3c16170965109b9ed059c5b0d57d0
Gianni D'Amico <damico@etud.insa-toulouse.fr>
2018-01-11 16:46:32 +0100

    [leptonRecognition] Affinage du threshold, diverses améliorations
    - erode et dilate 
    - Rectangle sur threshold et original
    - imshow() de 3 versions

--- pylepton-lepton3/pylepton_video.py
+++ pylepton-lepton3/pylepton_video.py
@@ -7,6 +7,14 @@ def main(device = "/dev/spidev0.0"):
     a = np.zeros((120, 160, 3), dtype=np.uint8)
     lepton_buf = np.zeros((120, 160, 1), dtype=np.uint16)
     fgbg = cv2.createBackgroundSubtractorMOG2()
+
+    # Création des fenêtres pour les agrandir 
+    cv2.namedWindow('Recognition',cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('Recognition',800,600)
+    cv2.namedWindow('Original',cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('Original',800,600)
+    cv2.namedWindow('Threshold', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('Threshold', 800,600)
     try:
         with Lepton3(device) as l:
             last_nr = 0
@@ -20,23 +28,32 @@ def main(device = "/dev/spidev0.0"):
                 np.right_shift(lepton_buf, 8, lepton_buf)
                 a[:lepton_buf.shape[0], :lepton_buf.shape[1], :] = lepton_buf
 
-                # Application BackgroundSubtractorMOG2
+                # Application du Background Subtractor MOG2
                 gmask = fgbg.apply(a)
 
-                thresh = cv2.threshold(gmask, 25, 255, cv2.THRESH_BINARY)[1]
-                thresh = cv2.dilate(thresh, None, iterations=2)
+                #Transformation de l'image sous format binaire puis affinage par erosion puis dilatation
+                thresh = cv2.threshold(gmask, 15, 255, cv2.THRESH_BINARY)[1]
+		kernel = np.ones((2,2), np.uint8)
+                thresh = cv2.erode(thresh, kernel, iterations=1)
+		thresh = cv2.dilate(thresh, kernel, iterations=1)
                 
-                # Recherche contours et traçage de rectangles pour les repérer
+                #Recherche de contours avec une surface supérieur à 70 pixels
                 _, contours, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
+                
                 for c in contours:
-                    if cv2.contourArea(c) < 100:
-                        continue
-
-                    (x,y,w,h) = cv2.boudingRect(c)
-                    cv2.rectangle(gmask, (x,y), (x + w, y + h), (255, 255, 255), 2)
-
-                cv2.imshow('Buffer',gmask)
+		# if the contour is too small, ignore it
+                    if cv2.contourArea(c) < 70:
+			continue
+ 
+		# compute the bounding box for the contour, draw it on the frame,
+		# and update the text
+                    (x, y, w, h) = cv2.boundingRect(c)
+                    cv2.rectangle(a, (x, y), (x + w, y + h), (255, 255, 255), 2)
+                    cv2.rectangle(thresh, (x, y), (x + w, y + h), (255, 255, 255), 2)
+		
+                cv2.imshow('Original',a)
+                cv2.imshow('Background Subtraction',gmask)
+		cv2.imshow('Threshold', thresh)
                 cv2.waitKey(1)
     except Exception:
         traceback.print_exc()
