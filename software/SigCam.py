45fd2517cb7fd687b72b3ef6398118f1e4e4b2f8
Gianni D'Amico <damico@etud.insa-toulouse.fr>
2018-01-15 14:48:46 +0100

    Nettoyage du git et commentaires mis en anglais pour Sigcam.py

--- software/SigCam.py
+++ software/SigCam.py
@@ -10,7 +10,7 @@ def main(device = "/dev/spidev0.0"):
     lepton_buf = np.zeros((120, 160, 1), dtype=np.uint16)
     fgbg = cv2.createBackgroundSubtractorMOG2()
 
-    # Création des fenêtres pour les agrandir 
+    # Window creation to expand them
     cv2.namedWindow('Recognition',cv2.WINDOW_NORMAL)
     cv2.resizeWindow('Recognition',800,600)
     cv2.namedWindow('Original',cv2.WINDOW_NORMAL)
@@ -31,25 +31,25 @@ def main(device = "/dev/spidev0.0"):
                 np.right_shift(lepton_buf, 8, lepton_buf)
                 a[:lepton_buf.shape[0], :lepton_buf.shape[1], :] = lepton_buf
 
-                # Application du Background Subtractor MOG2
+                # Background Removing by appliying the BackgroundSubtractorMOG2
                 gmask = fgbg.apply(a)
 
-                #Transformation de l'image sous format binaire puis affinage par erosion puis dilatation
+                # Image processing in binary format then refining it by erosion then dilatation
                 thresh = cv2.threshold(gmask, 15, 255, cv2.THRESH_BINARY)[1]
                 kernel = np.ones((2,2), np.uint8)
                 thresh = cv2.erode(thresh, kernel, iterations=1)
                 thresh = cv2.dilate(thresh, kernel, iterations=1)
                 
-                #Recherche de contours avec une surface supérieur à 70 pixels
+                #Finding all the contours with an area bigger than 70 pixels
                 _, contours, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
                 
                 for c in contours:
-		# if the contour is too small, ignore it
+                    # if the contour is too small, ignore it
                     if cv2.contourArea(c) < 70:
                         continue
  
-		# compute the bounding box for the contour, draw it on the frame,
-		# and update the text
+                    # compute the bounding box for the contour, draw it on the frame,
+                    # and update the text
                     (x, y, w, h) = cv2.boundingRect(c)
                     cv2.rectangle(a, (x, y), (x + w, y + h), (255, 255, 255), 2)
                     cv2.rectangle(thresh, (x, y), (x + w, y + h), (255, 255, 255), 2)
@@ -58,6 +58,8 @@ def main(device = "/dev/spidev0.0"):
                         #serial_transmission.serialsending("Detected","/dev/ttyUSB0")
                         print "Envoyé !"
                         rectangleFound = True
+                
+                # Displays the 3 steps of the image processing in 3 windows
                 cv2.imshow('Original',a)
                 cv2.imshow('Background Subtraction',gmask)
                 cv2.imshow('Threshold', thresh)
